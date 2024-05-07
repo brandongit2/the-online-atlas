31dc04242062826bf0ec91fd5fe5269c02f7c206
Brandon Tsang <brandononline2@gmail.com>
2024-05-07 14:06:15 -0400

    include built wasm files

---
+++ rust-lib/build/the_online_atlas_rust_lib.js
@@ -0,0 +1,4 @@
+import * as wasm from "./the_online_atlas_rust_lib_bg.wasm";
+import { __wbg_set_wasm } from "./the_online_atlas_rust_lib_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./the_online_atlas_rust_lib_bg.js";
